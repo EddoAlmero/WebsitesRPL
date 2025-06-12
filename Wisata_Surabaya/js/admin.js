153f93ddfc10990f8f76ba32a9d1b9531d8dab2b
EddoAlmero <90173663+EddoAlmero@users.noreply.github.com>
2025-06-13 01:20:12 +0700

    Update admin.js

--- Wisata_Surabaya/js/admin.js
+++ Wisata_Surabaya/js/admin.js
@@ -6,7 +6,6 @@ const firebaseConfig = {
   storageBucket: "wisata-surabaya-652ff.firebasestorage.app",
   messagingSenderId: "205207573499",
   appId: "1:205207573499:web:0ff0a5248ab4563ea3315e"
-  measurementId: "G-6P6VQY1SLK"
 };
 
 // Initialize Firebase
